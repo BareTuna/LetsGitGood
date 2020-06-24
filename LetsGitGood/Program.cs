c5dbfbed8f75ef3f553846a4032f9eb2986fe809
Tanner Hansen <thechumstudios@gmail.com>
2020-06-24 11:53:47 -0600

    First commit

---
+++ LetsGitGood/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LetsGitGood
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World?");
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
