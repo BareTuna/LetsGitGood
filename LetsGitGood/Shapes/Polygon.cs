582a78a6d5dd1a12896c8572d46a1478ff4536d8
Tanner Hansen <thechumstudios@gmail.com>
2020-06-25 15:53:28 -0600

    Added Rectangle and fixed Polygon namespace

--- LetsGitGood/Shapes/Polygon.cs
+++ LetsGitGood/Shapes/Polygon.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Shapes
+namespace LetsGitGood
 {
     abstract class Polygon
     {
---
+++ LetsGitGood/Shapes/Rectangle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LetsGitGood
+{
+    class Rectangle : Polygon
+    {
+        public override int NumberOfSides
+        {
+            get { return 4; }
+        }
+        public double Length { get; set; }
+        //public double Width { get { return Length; } set { Length = value; } }
+        public double Height { get; set; }
+
+        public override double Area
+        {
+            get { return Length * Height; }
+        }
+        public override double Perimeter
+        {
+            get { return 2 * (Length + Height); }
+        }
+
+    }
+}
